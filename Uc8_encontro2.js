ba246791ab74e151f0897cf364cb252a6de693f6
h <henriquesamu3l@gmaim.com>
2022-02-28 10:13:25 -0300

    trabalho

---
+++ Uc8_encontro2.js
@@ -0,0 +1,18 @@
+let numDeAlunos = 10;
+
+for (let contador = 0; contador <= numDeAlunos; contador ++)
+{
+    console.log(contador);
+    if (contador == 0){
+        console.log('O número atual é zero');
+    } else if  (contador % 2 == 0){
+        console.log('O númeor é par!');
+    } else {
+        console.log('O número é impar!');}
+
+    }
+
+
+
+
+        
